4108ad3d348e9f2816b789f1c70d77254c6daaf5
Christian <christiandr@titamedia.com>
2020-12-23 07:33:55 -0500

    Primer commit del curso react.js desde cero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
+import Curso from './curso';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+  <div className="main-banner img-container l-section" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+      <div className="lg-cols-4 lg-x-2">
+        <img className="main-banner__img" src="https://image.freepik.com/vector-gratis/fondo-ciudad-noche-luces-neon_1441-2822.jpg" alt="banner"/>
+        <div className="main-banner__data s-center">
+          <p className="t2 s-mb-0">Cursos de EDteam</p>
+          <p>Tu futuro te está esperando</p>
+          <a href="https://ed.team" className="button">Suscribirse</a>
+        </div>
+      </div>
     </div>
-  );
-}
+  </div>
+  <div className="ed-grid m-grid-3">
+    <Curso/>
+    <Curso/>
+    <Curso/>
+    <Curso/>
+    <Curso/>
+    <Curso/>
+  </div>
+  </>
+)
+
 
 export default App;
+
+// Reglas JSX
+
+// 1. Toda etiqueta debe cerrarse
+// 2. Los componentes deben devolver un solo elemento padre
+// 3. Apoyarse de los fragments cuando necesito devolver dos o más elementos 
+// 4. <Fragment> Hijos </Fragment> = <> Hijos </>
+// 5. img siempre se cierra <img src="url" />
+// 6. class => className
+// 7. for => htmlFor
+// 8. NO if, else, while
